85a19b7b59f4de4b4552ea87014bb60a9f6971f0
Minsik Kim <ppp37686@gmail.com>
2023-11-11 16:10:38 +0900

    Add LeftBox

---
+++ blogclone/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import styled from "styled-components";
+
+const LeftBox = styled.div`
+	width: 30%;
+  height: 100vh;
+  background-color: #252525;
+`;
+
+function App() {
+  return (
+    <LeftBox>ㅇㅇ</LeftBox>
+  );
+}
+
+
+
+export default App;
